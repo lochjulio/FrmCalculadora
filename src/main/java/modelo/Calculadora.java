7a9388bae756c4e03b32561d74c51a80693834a7
julioloch <juliocesarlochstupp@gmail.com>
2024-05-15 23:25:57 -0300

    FrmCalculadora

---
+++ src/main/java/modelo/Calculadora.java
@@ -0,0 +1,21 @@
+package modelo;
+
+import visao.FrmCalculadora;
+
+public class Calculadora {
+
+    public static void main(String[] args) {
+        // Instância a interface gráfica
+        FrmCalculadora janela = new FrmCalculadora();
+        // Torna a janela visível
+        janela.setVisible(true);     
+    }
+
+    public void setjTextField1(double parseDouble) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public void setjTextField2(double parseDouble) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+}
